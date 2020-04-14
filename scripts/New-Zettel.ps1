40b3e46ce9a91f6d5bdce3d843137da9b080c06e
Anton Burger <burgeranton@gmail.com>
2020-04-14 23:29:19 +0100

    Reorganise to having a dist directory
    The aim is to eventually build dist
    from the scripts and other tools in
    this repo

---
+++ scripts/New-Zettel.ps1
@@ -0,0 +1,47 @@
+param
+(
+    [Parameter(Mandatory)]
+    [string]
+    $Title,
+    [string]
+    $ZettelDir = "$PSScriptRoot/.."
+)
+
+function Get-MaxZettelId($dir) {
+    if (-not (Test-Path -Path $dir -PathType Container)) {
+        return 0
+    }
+    $zettels = Get-ChildItem $dir -Filter "*.md"
+    $max = @($zettels.BaseName) -match "\d+" | Measure-Object -Maximum
+    return [int]$max.Maximum
+}
+
+function Get-ZettelId($zettelDir) {
+    $now = Get-Date
+    $culture = [System.Globalization.CultureInfo]::InvariantCulture
+    $week = $culture.Calendar.GetWeekOfYear($now, [System.Globalization.CalendarWeekRule]::FirstFourDayWeek, [System.DayOfWeek]::Monday)
+    $subdir = "{0:yy}/$week" -f $now
+    $dir = Join-Path $zettelDir $subdir
+    $next = 1 + (Get-MaxZettelId($dir))
+    "$subdir/" + ("{0:D2}" -f $next)
+}
+
+if (-not (Test-Path -Path $ZettelDir -PathType Container)) {
+    throw "Not a directory: $ZettelDir"
+}
+
+$ZettelDir = Resolve-Path $ZettelDir
+$zettelId = Get-ZettelId($ZettelDir)
+$dir = Join-Path $ZettelDir (Split-Path -Path $zettelId -Parent)
+$null = New-Item $dir -ItemType Directory -Force
+$file = Join-Path $ZettelDir "$zettelId.md"
+@"
+---
+title: $Title
+created: $("{0:s}" -f (Get-Date))
+tags:
+---
+
+# $Title
+"@ | Set-Content $file
+[string](Get-ChildItem $file)
