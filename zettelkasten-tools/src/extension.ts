ff8b25ae8cd9d5e648d4302d29e5f7de3bc50d84
Anton Burger <burgeranton@gmail.com>
2020-06-05 23:41:36 +0100

    Add language server support
    At the moment, the language server lives in another repo and the path is hardcoded.
    This will probably have to change at some point...

--- zettelkasten-tools/src/extension.ts
+++ zettelkasten-tools/src/extension.ts
@@ -1,12 +1,37 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient';
 import pasteAttachment from './paste_attachment';
 import NoteDefinitionProvider from './note_definition_provider';
+
+let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const serverOptions: ServerOptions = {
+        command: "C:\\Users\\Anton\\Code\\zettel-language-server\\zettel-language-server\\bin\\Debug\\netcoreapp3.1\\zettel-language-server.exe"
+,
+        transport: TransportKind.stdio
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "markdown" }],
+        synchronize: {
+            fileEvents: vscode.workspace.createFileSystemWatcher("**/.md")
+        }
+    };
+
+    client = new LanguageClient("zettelkastenTools", "Zettelkasten Tools", serverOptions, clientOptions);
+
+    client.start();
+
     context.subscriptions.push(vscode.languages.registerDefinitionProvider('markdown', new NoteDefinitionProvider()));
     context.subscriptions.push(vscode.commands.registerTextEditorCommand('zettelkasten-tools.pasteAttachment',
         async editor => {
@@ -19,4 +44,8 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    if (client) {
+        return client.stop();
+    }
+}
